fbf8d778ac2ba739fdf12297c908530609d12134
Kelly <kellyshao728@gmail.com>
2022-11-23 01:35:26 -0800

    Modal Setup

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
+import NavBar from './components/Navigation/NavBar';
 // import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
+import UsersList from './components/User/UsersList';
+import User from './components/User/User';
 import { authenticate } from './store/session';
 
 function App() {
