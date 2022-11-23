29c8f3994a163ae04628040d2628195e53f971d9
Kelly <kellyshao728@gmail.com>
2022-11-22 23:38:08 -0800

    testing

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -3,9 +3,6 @@ from wtforms import StringField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
-def test():
-    pass
-
 def user_exists(form, field):
     # Checking if user exists
     email = field.data
