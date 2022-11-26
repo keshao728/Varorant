c5c0b36ca4fb29875e98805725ba7f869cb957f6
Kelly <kellyshao728@gmail.com>
2022-11-25 23:53:22 -0800

    Ticket Store/Seeder Working

--- react-app/src/components/Ticket/Ticket.js
+++
@@ -1,7 +0,0 @@
-import {getAllTicketsThunk} from '../../store/ticket';
-import { useDispatch } from 'react-redux';
-
-const Ticket = ({ticket}) => {
-  const dispatch = useDispatch();
-  
-}
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import ticketReducer from './ticket';
+import mediaReducer from './media';
 
 const rootReducer = combineReducers({
   session,
+  ticket: ticketReducer,
+  media: mediaReducer
 });
 
 
